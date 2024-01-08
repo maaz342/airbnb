73e5989f94b4d57f9d4669e108f6e102eb9fb8a8
chames@gmail.com <maazkamran72@gmail.com>
2024-01-08 20:52:57 +0500

    asd

--- app/hooks/useFavorite.ts
+++ app/hooks/useFavorite.ts
@@ -1,31 +1,36 @@
 import axios from "axios";
-import {useRouter} from "next/navigation";
-import {useCallback, useMemo} from "react";
-import {toast} from "react-hot-toast";
-import {SafeUser} from "../types";
+import { useRouter } from "next/navigation";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+
+import { SafeUser } from "@/app/types";
+
 import useLoginModal from "./useLoginModal";
-interface IUseFavorite{
-listingId: string;
-currentUser: SafeUser|null;
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null
 }
-const useFavorite=({
-    listingId,
-    currentUser
-}:IUseFavorite)=>{
-    const router=useRouter();
-    const loginModal=useLoginModal();
-    const hasFavorited=useMemo(()=> {
-    const list=currentUser?.favoriteIds || [];
-    return list.includes(listingId); }, [currentUser,listingId]);
-    const toggleFavorite=useCallback(async (
-    e: React.MouseEvent <HTMLDivElement>
-    )=>{
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(async (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
+
     if (!currentUser) {
-        return loginModal.onOpen();
+      return loginModal.onOpen();
     }
 
-try {
+    try {
       let request;
 
       if (hasFavorited) {
